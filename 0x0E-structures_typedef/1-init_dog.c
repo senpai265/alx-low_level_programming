bbbac9ff0dc1b1e0c0e2879c0ae400b1ecd1aa22
Steven Nyauti <115984381+senpai265@users.noreply.github.com>
2023-01-16 22:39:45 +0200

    done

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,12 +1,14 @@
-#include "doh.h"
+#include "dog.h"
 
 /**
- *  int_dog - inits a variable of
- *type dog
- *@d: dog identification
- *@name: name of dog
- *@age: age of dog
- *@owner: owner's name
+  int_dog - inits a variable of
+ type dog
+ @d: dog identification
+ @name: name of dog
+ @age: age of dog
+ @owner: owner's name
+ 
+ Description: Long
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
@@ -17,4 +19,3 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 		(*d).owner = owner;
 	}
 }
-
