f8ba3812e23ec2911844c71586c6ad941e92f5a5
Steven Nyauti <115984381+senpai265@users.noreply.github.com>
2023-01-16 23:03:27 +0200

    Update 5-free_dog.c

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,30 +1,18 @@
 #include "dog.h"
 
 /**
+ * free_dog - Frees dog
+ * @d: dog to be freed
  *
- * *free_dog - Frees dog
- *
- * *@d: dog to be freed
- *
- * *
- *
- * */
+ */
 
 void free_dog(dog_t *d)
-
 {
 
 	if (d == NULL)
-
 		return;
-
-
-
 	free(d->name);
-
 	free(d->owner);
-
 	free(d);
 
 }
-
