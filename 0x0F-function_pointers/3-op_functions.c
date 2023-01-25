8c83b7e4ad3effb6de6e0032ad6216e411843d93
senpai265 <stevenyauti@gmail.com>
2023-01-25 07:05:28 +0200

    correction

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,163 +1,86 @@
 #include <stdio.h>
-
 #include "3-calc.h"
 
-
-
 /**
  *
- *  * op_add - function adds two integers
+ * op_add - function adds two integers
+ * @a: integer
+ * @b: integer
  *
- *   * @a: integer
- *
- *    * @b: integer
- *
- *     *
- *
- *      * Return: resultant operation
- *
- *      */
-
-
+ * Return: resultant operation
+ */
 
 int op_add(int a, int b)
-
 {
-
 	return (a + b);
-
 }
 
 
 
 /**
+ * op_sub - function substrate two integers
+ * @a: integer
+ * @b: integer
  *
- *  * op_sub - function substrate two integers
- *
- *   * @a: integer
- *
- *    * @b: integer
- *
- *     *
- *
- *      * Return: resultant operation
- *
- *      */
-
-
+ * Return: resultant operation
+ */
 
 int op_sub(int a, int b)
-
 {
-
 	return (a - b);
-
 }
 
-
-
 /**
+ * op_mul - function multiplies  two integers
  *
- *  * op_mul - function multiplies  two integers
+ * @a: integer
+ * @b: integer
  *
- *   *
+ * Return: resultant operation
  *
- *    * @a: integer
- *
- *     * @b: integer
- *
- *      *
- *
- *       * Return: resultant operation
- *
- *       */
-
-
+ */
 
 int op_mul(int a, int b)
-
 {
-
 	return (a * b);
-
 }
 
-
-
 /**
+ * op_div - function division
  *
- *  * op_div - function division
+ * @a: integer
+ * @b: integer
  *
- *   *
- *
- *    * @a: integer
- *
- *     * @b: integer
- *
- *      *
- *
- *       * Return: Return the result of the operation
- *
- *        */
-
-
+ * Return: Return the result of the operation
+ */
 
 int op_div(int a, int b)
-
 {
-
 	if (b == 0)
-
 	{
-
 		printf("Error\n");
-
 		exit(100);
-
 	}
-
-
 
 	return (a / b);
-
 }
 
-
-
 /**
+ * op_mod - function modulo
  *
- *  * op_mod - function modulo
+ * @a: integer
+ * @b: integer
  *
- *   *
- *
- *    * @a: integer
- *
- *     * @b: integer
- *
- *      *
- *
- *       * Return: Return the result of the operation
- *
- *        */
-
-
+ * Return: Return the result of the operation
+ */
 
 int op_mod(int a, int b)
-
 {
-
 	if (b == 0)
-
 	{
-
 		printf("Error\n");
-
 		exit(100);
-
 	}
 
-
-
 	return (a % b);
-
 }
