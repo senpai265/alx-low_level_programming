9aa7786978b5f2875915b72a06b59da7b3a7aa76
senpai265 <stevenyauti@gmail.com>
2023-01-25 06:42:15 +0200

    correction

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -49,6 +49,11 @@ int op_mul(int a, int b)
 
 int op_div(int a, int b)
 {
+	if (b == 0)
+	{
+	printf("Error\n");
+	exit(100);
+	}
 	return (a / b);
 }
 
@@ -62,6 +67,11 @@ int op_div(int a, int b)
 
 int op_mod(int a, int b)
 {
+	if (b == 0);
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a % b);
 }
 
